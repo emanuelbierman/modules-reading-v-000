6dbf512c05347c019827bef29b54d5aadade939a
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-05-30 18:47:22 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,6 +1,6 @@
 
 module Dance
-  
+
   def twirl
     "I'm twirling!"
   end
@@ -16,4 +16,5 @@ module Dance
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
   end
+  
 end
